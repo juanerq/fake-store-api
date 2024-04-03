d7ddb77778309207f8b4ffa4bfcf484f2c9e253e
juanerq <jrjuanreyes64@gmail.com>
2024-04-02 19:58:23 -0500

    📝 role entity documentation

--- src/context/security/roles/entities/role.entity.ts
+++ src/context/security/roles/entities/role.entity.ts
@@ -9,12 +9,22 @@ import {
 } from 'typeorm';
 import { RolePermissions } from './role-permissions.entity';
 import { User } from '../../../users/entities';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('roles')
 export class Role {
+  @ApiProperty({
+    type: 'number',
+    example: 12,
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    type: 'string',
+    required: true,
+    example: 'ADMIN',
+  })
   @Column({
     type: 'varchar',
     unique: true,
